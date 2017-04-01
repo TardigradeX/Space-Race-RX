f005149ca4255e096298908e4f37641ea66be483
Michael Rauer <michi.rauer@googlemail.com>
2017-04-01 20:21:59 +0200

    [SRRX-9]  Managing clients server.py accepts connections from controller.py and master_test.py. First connect master, retrieve <roomid>, use <roomid> for controller to connect to server.py

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,7 @@
 import sys
 import random
+import re
+
 
 from twisted.web.static import File
 from twisted.python import log
@@ -11,81 +13,112 @@ from autobahn.twisted.websocket import WebSocketServerFactory, \
 
 from autobahn.twisted.resource import WebSocketResource
 
-class SpaceRaceRXProtocol(WebSocketServerProtocol):
-
-    def onConnect(self, response):
-        print("Connected to Server: {}".format(response.peer))
-
-    def onOpen(self):
-        """
-        Connection from client is opened. Fires after opening
-        websockets handshake has been completed and we can send
-        and receive messages.
-
-        Register client in factory, so that it is able to track it.
-        Try to find conversation partner for this client.
-        """
-        self.factory.register(self)
-        self.factory.findPartner(self)
-
-    def connectionLost(self, reason):
-        """
-        Client lost connection, either disconnected or some error.
-        Remove client from list of tracked connections.
-        """
-        self.factory.unregister(self)
-
-    def onMessage(self, payload, isBinary):
-        """
-        Message sent from client, communicate this message to its conversation partner,
-        """
-        self.factory.communicate(self, payload, isBinary)
-
-    def onClose(self):
-        print("Connection closed")
-
-
 class SpaceRaceRXFactory(WebSocketServerFactory):
 
     def __init__(self, *args, **kwargs):
         super(SpaceRaceRXFactory, self).__init__(*args, **kwargs)
-        self.clients = {}
+        # since connection order is 1) master and 2) controller, this works.
+        # Room can be prepared with empty controllers,
+        # destroyed as last controller gone
+        self.rooms = {} # room peer with ID
+        self.ctrl = {} # client with peer with room id
+        self.match = {} # room id with (client id)*4
+        self.roomuid = -1
 
-    def register(self, client):
-        """
-        Add client to list of managed connections.
-        """
-        self.clients[client.peer] = {"object": client, "partner": None}
+    def registerMaster(self, client):
+        if client not in self.rooms:
+            print("registered client {}".format(client.peer))
+            self.roomuid += 1
+            self.rooms[client.peer] = str(self.roomuid).zfill(4)
+            self.match[self.rooms[client.peer]] = 0
+            return(self.rooms[client.peer])
+
+    def registerController(self, client, roomid):
+        roomfree = self.checkRoom(roomid)
+        if not roomfree:
+            self.sendMessage2("Room is not available, either full or it does not exist")
+            pass # restart login ask for another room id
+
+        if client not in self.rooms:
+            print("registered client {}".format(client.peer))
+            self.match[roomid] += 1
+            self.ctrl[client.peer] = roomid
+
+    def checkRoom(self, roomid):
+        if not roomid in self.match: ## room id must exist
+            return 1
+        if self.match[roomid] < 4: ## max 4 players
+            return 2
+        return 0
 
     def unregister(self, client):
         """
         Remove client from list of managed connections.
         """
-        self.clients.pop(client.peer)
+        print("Room unregistered:" + str(client.peer))
+        try:
+            self.clients.pop(client.peer)
+        except:
+            print("Nothing to unregister: Client " + client.peer + \
+                " not registered")
 
-    def findPartner(self, client):
-        """
-        Find chat partner for a client. Check if there any of tracked clients
-        is idle. If there is no idle client just exit quietly. If there is
-        available partner assign him/her to our client.
-        """
-        available_partners = [c for c in self.clients if c != client.peer and not self.clients[c]["partner"]]
-        if not available_partners:
-            print("no partners for {} check in a moment".format(client.peer))
-        else:
-            partner_key = random.choice(available_partners)
-            self.clients[partner_key]["partner"] = client
-            self.clients[client.peer]["partner"] = self.clients[partner_key]["object"]
+    def broadcast(self, msg):
+        print("broadcasting message '{}' ..".format(msg))
+        for c in self.clients:
+            c.sendMessage(msg.encode('utf8'), False)
+            print("message sent to {}".format(c.peer))
 
-    def communicate(self, client, payload, isBinary):
-        """
-        Broker message from client to its partner.
-        """
-        c = self.clients[client.peer]
-        if not c["partner"]:
-            c["object"].sendMessage("Sorry you dont have partner yet, check back in a minute".encode('utf8'), isBinary=False)
-        else:
-            c["partner"].sendMessage(payload, isBinary)
+class SpaceRaceRXProtocol(WebSocketServerProtocol):
+
+    def parseLogin(self, payload):
+        print(payload)
+        pattern = re.compile(u'<.+>:.+')
+        if bool(pattern.search(payload)):
+            c = payload.split(":")
+            if c[0] == "<loginrequest>":
+                if c[1] == 'master':
+                    print('Registering master')
+                    roomuid = self.factory.registerMaster(self)
+                    self.sendMessage2("<logingranted>:master:"+ roomuid)
+
+                elif c[1] == 'controller':
+                    print('Registering controller for room ' + c[2])
+                    self.factory.registerController(self, c[2])
+                    self.sendMessage2("<logingranted>:controller:room" +c[2])
+            else:
+                print('Command not recognized')
+                self.sendMessage2("login denied")
+                self.close()
+
+    def onConnect(self, request):
+        print("Connected to Server: {}".format(request.peer))
+        print(request)
+        print(type(request))
+        print(dir(request))
+
+    # def onOpen(self, request):
+    #     print(request)
+
+    def connectionLost(self, reason):
+        self.factory.unregister(self)
+
+    def onMessage(self, payload, isBinary):
+        pay1 = payload.decode('utf8')
+        print(self.peer + ": " + pay1)
+        self.parseLogin(pay1)
+
+    def sendMessage2(self, payload):
+        self.sendMessage(payload.encode('utf8'), False)
+
+    def sendClose(self, code = None, reason = None):
+        print("Announce closing with " + self.peer)
+        """self.sendClose(code, reason)"""
+
+    def onClose(self):
+        print("Closing connection")
+        """self.onClose()"""
+
+
 
 
 if __name__ == "__main__":
